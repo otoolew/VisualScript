1aff01ff0087a40ac2b4188ddafbbbd47dc56e16
O'Toole <WBO5@pitt.edu>
2019-01-17 18:24:58 -0500

    init commit

---
+++ VisualScript/wwwroot/js/vpcase-diagram.js
@@ -0,0 +1,29 @@
+﻿function CaseDiagram() {
+    this.case_diagram = '';
+    this.case_nodes = [];
+}
+
+
+CaseDiagram.prototype.init = function (opts) {
+    // cache references to the DOM elements we need to manage
+    this.$case_diagram = $("<svg></svg>").addClass('caseDiagram');
+
+    this.$case_nodes.prepend(
+        $("<div></div>")
+            .addClass("caseNode")
+            .text("New Node")
+    );
+};
+
+var vpCaseDiagram = new CaseDiagram();
+
+$(document).ready(function () {
+    vpCaseDiagram.init({
+        case_nodes: "#caseNode"
+    });
+});
+
+//    this.svg = '';
+//this.svg.ns = '';
+//this.case_id = '';
+//this.case_nodes = [];
---
+++ VisualScript/wwwroot/js/vpcase-node.js
@@ -0,0 +1,10 @@
+﻿var CaseNode = (function () {
+    function addNote(note) {
+        $notes.prepend(
+            $("<a href='#'></a>")
+                .addClass("note")
+                .text(note)
+        );
+    }
+
+})();
